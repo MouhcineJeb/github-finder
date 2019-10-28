60109061362710512458dd5beb92ce8332aeec77
abc <abc@example.com>
2019-10-28 19:52:46 +0100

    Navbar and Users added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Mouhcine';
-    const loading = false;
-    const showName = true;
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
